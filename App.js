1d290f13c1ad8e95d29d47168257bd76f4999e15
Paul-Nathan <pngotte@gmail.com>
2024-10-11 12:34:55 +0100

    Screens setup

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Review App</Text>
-    </View>
+      <Home/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    
-  }
-});
