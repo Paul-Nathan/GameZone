4044b90d2691d94dfa4c8d75a9ed1fb62d28337c
Paul-Nathan <pngotte@gmail.com>
2024-10-17 11:57:09 +0100

    yup installation done and Constraints established succesfully

--- screens/ReviewForm.js
+++ screens/ReviewForm.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import { Formik } from 'formik';
-import { View, StyleSheet, Text, TextInput } from 'react-native';
+import { View, StyleSheet, Text, TextInput, Button} from 'react-native';
 import {GlobalStyles} from'../styles/Global';
+import * as yup from 'yup';
+
+const ReviewSchema = yup.object({
+    title: yup.string()
+        .required()
+        .min(4),
+    body: yup.string()
+        .required()
+        .min(8),
+    rating: yup.string()
+        .required()
+        .test('is-num-1-5', 'Rating must be a number 1-5', (val) => {
+            return parseInt(val) < 6 && parseInt(val) > 0;
+        })
+})
 
 export default function ReviewForm({addReview}) {
   return (
     <View style={GlobalStyles.container}>
         <Formik
         initialValues={{title: '', body:'', rating:''}}
+        validationSchema={ReviewSchema}
         onSubmit={(values) => {
             addReview(values)
         }}
